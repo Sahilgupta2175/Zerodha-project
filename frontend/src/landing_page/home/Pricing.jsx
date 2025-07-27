bc465ce997d3914411ffba0395186b0ab8037686
Sahil Gupta <guptasahil2175@gmail.com>
2025-07-27 23:25:18 +0530

    Feat: Implement UI for the education section
    Builds out the education section with a two-column layout, including an image, descriptive text, and links to educational resources.
    
    Also, applies minor spacing and font size adjustments to the pricing section for better visual consistency.

--- frontend/src/landing_page/home/Pricing.jsx
+++ frontend/src/landing_page/home/Pricing.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 function Pricing() {
     return (
-        <div className="container">
+        <div className="container mb-5">
             <div className="row">
                 <div className="col-4">
-                    <h1 className='mb-3'>Unbeatable pricing</h1>
+                    <h1 className='mb-3 fs-2'>Unbeatable pricing</h1>
                     <p>We pioneered the concept of discount broking and price transparency in India. Flat fees and no hidden charges.</p>
                     <a href="" style={{textDecoration:"none"}}>See Pricing <i class="fa-solid fa-arrow-right-long"></i></a>
                 </div>
                 <div className="col-2"></div>
-                <div className="col-6">
+                <div className="col-6 mb-5">
                     <div className="row text-center">
                         <div className="col-6 border p-3">
                             <h1 className='mb-3'>₹0</h1>
